e6c309d13e7f7adfd756487049556dfaf70fdfbb
Devyzr <daniel.e.verdin@gmail.com>
2018-05-22 10:48:16 -0500

    A few minor changes to multihash

--- src/multihash.go
+++ src/multihash.go
@@ -3,77 +3,84 @@
 package main
 
 import (
-    "crypto/sha256"
-    "encoding/hex"
-    "io"
-    "io/ioutil"
-    "log"
-    "os"
-    "strings"
+	"crypto/sha256"
+	"encoding/hex"
+	"io"
+	"io/ioutil"
+	"log"
+	"os"
+	"sort"
 )
 
 func main() {
-    log.Println("Empezando a generar firmas\n")
 
-    all_files := getFiles()
+	f, err := os.OpenFile("hash_log.txt", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+	if err != nil {
+		log.Fatal("Error abriendo archivo de log. ", err)
+	}
+	defer f.Close()
 
-    for _, filename := range all_files {
-        name_only_parts := strings.Split(filename, "/")
-        name_only := name_only_parts[len(name_only_parts)-1]
-        file_hash := hash_256(filename)
-        log.Printf("\nGenerando huella para '%s'\nHuella: %s\n\n", name_only, file_hash)
-    }
+	log.SetOutput(f)
 
-    log.Printf("Se finalizó el proceso de generación de %v firmas con éxito\n", len(all_files))
+	log.Println("Empezando a generar firmas\n")
+
+	all_files := getFiles()
+	sort.Strings(all_files)
+
+	for _, filename := range all_files {
+		file_hash := hash_256(filename)
+		log.Printf("\nGenerando huella para '%s'\nHuella: %s\n\n", filename, file_hash)
+	}
+
+	log.Printf("Se finalizó el proceso de generación de %v firmas con éxito\n", len(all_files))
 }
 
 func getFiles() []string {
-    var allDirs, allFiles []string
-    var currentDir string
-    var dirNameToSave, fileNameToSave string
+	var allDirs, allFiles []string
+	var currentDir string
+	var dirNameToSave, fileNameToSave string
 
-    allDirs = append(allDirs, ".")
+	allDirs = append(allDirs, ".")
 
-    for len(allDirs) > 0 {
-        currentDir = allDirs[0]
-        dirContents, err := ioutil.ReadDir(currentDir)
-        // Error checking
-        if err != nil {
-            log.Fatal(err)
-        } else {
-            // Separate between files and dirs
-            for _, dirElem := range dirContents {
-                if dirElem.IsDir() {
-                    dirNameToSave = currentDir + "/" + dirElem.Name()
-                    allDirs = append(allDirs, dirNameToSave)
-                } else {
-                    fileNameToSave = currentDir + "/" + dirElem.Name()
-                    allFiles = append(allFiles, fileNameToSave)
-                }
-            }
-        }
-        // Slice out the file we already used
-        allDirs = allDirs[1:]
-    }
+	for len(allDirs) > 0 {
+		currentDir = allDirs[0]
+		dirContents, err := ioutil.ReadDir(currentDir)
+		// Error checking
+		if err != nil {
+			log.Fatal(err)
+		} else {
+			// Separate between files and dirs
+			for _, dirElem := range dirContents {
+				if dirElem.IsDir() {
+					dirNameToSave = currentDir + "/" + dirElem.Name()
+					allDirs = append(allDirs, dirNameToSave)
+				} else {
+					fileNameToSave = currentDir + "/" + dirElem.Name()
+					allFiles = append(allFiles, fileNameToSave)
+				}
+			}
+		}
+		// Slice out the file we already used
+		allDirs = allDirs[1:]
+	}
 
-    return allFiles
+	return allFiles
 }
 
-
 func hash_256(filename string) string {
-    file_to_hash, err := os.Open(filename)
-    if err != nil {
-        log.Fatal("Error abriendo archivo. ", err)
-    }
-    defer file_to_hash.Close()
+	file_to_hash, err := os.Open(filename)
+	if err != nil {
+		log.Fatal("Error abriendo archivo. ", err)
+	}
+	defer file_to_hash.Close()
 
-    hasher := sha256.New()
+	hasher := sha256.New()
 
-    _, err = io.Copy(hasher, file_to_hash)
-    if err != nil {
-        log.Fatal("Error error copiando archvo al generador de hash. ", err)
-    }
+	_, err = io.Copy(hasher, file_to_hash)
+	if err != nil {
+		log.Fatal("Error error copiando archvo al generador de hash. ", err)
+	}
 
-    sha := hex.EncodeToString(hasher.Sum(nil))
-    return sha
+	sha := hex.EncodeToString(hasher.Sum(nil))
+	return sha
 }
